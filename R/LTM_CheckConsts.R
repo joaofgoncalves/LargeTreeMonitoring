d392bafb1c601820f00580024d3df865a61002bd
joao_goncalves <joaofgo@gmail.com>
2026-04-09 21:07:26 +0100

    Modified constants to avoid global vars

--- R/LTM_CheckConsts.R
+++ R/LTM_CheckConsts.R
@@ -1,7 +1,7 @@
 
-SPECTRAL_INDICES_LIST <<- c("NDVI","EVI","NBR","NDRE","EVI2")
+SPECTRAL_INDICES_LIST <- c("NDVI","EVI","NBR","NDRE","EVI2")
 
-PROC_LEVELS_LIST <<- c("L1","L1C","L2","L2A")
+PROC_LEVELS_LIST <- c("L1","L1C","L2","L2A")
 
-VALID_DATA_TYPES <<- c("spi", "spi_mov_wind", "spi_smooth", "spi_mov_smooth")
+VALID_DATA_TYPES <- c("spi", "spi_mov_wind", "spi_smooth", "spi_mov_smooth")
 
